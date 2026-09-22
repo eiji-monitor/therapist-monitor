6992d3dea670310e4996d12970038ccb44acadfd
eiji-monitor <eiji12221@gmail.com>
2026-09-22 15:46:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
+self.addEventListener("install", event => { self.skipWaiting(); });
+self.addEventListener("activate", event => { event.waitUntil(clients.claim()); });
+
 self.addEventListener("push", event => {
   let data = {};
   try { data = event.data ? event.data.json() : {}; }
